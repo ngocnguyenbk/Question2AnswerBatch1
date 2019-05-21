44e376d060b5a3636ab9b5d665651b054a0209dc
Linh <tomroket@gmail.com>
2019-05-21 18:46:06 +0800

    Add unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :username,  presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :username,  presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates :fullname, presence: true, length: { minimum: 20 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
