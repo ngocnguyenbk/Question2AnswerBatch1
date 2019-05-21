958769a1e6000791085c7520a5eefd87f4bd776d
Linh <tomroket@gmail.com>
2019-05-21 18:42:59 +0800

    add unique user, enum role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  validates :username,  presence: true, length: { maximum: 50 }
-  validates :fullname, presence: true, length: { minimum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates :username,  presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :fullname, presence: true, length: { minimum: 20 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+  enum role: [:manager, :member]
 end
