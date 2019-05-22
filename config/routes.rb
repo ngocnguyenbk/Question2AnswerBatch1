9ed23c436d00e359920cc99a3cfafb9b857a131a
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-22 13:36:01 +0700

    create-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new'
+
+  get '/create', to: 'sessions#create'
+
+  get '/destroy', to: 'sessions#destroy'
+
+ 
+
+  root 'static_page#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
