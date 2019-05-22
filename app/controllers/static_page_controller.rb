88bfc77480e9e18e54308783884fbdef5c899baa
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-22 16:48:31 +0700

    create layout

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,4 +1,4 @@
 class StaticPageController < ApplicationController
-  def home
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/login', to: 'sessions#new'
-
-  get '/create', to: 'sessions#create'
-
-  get '/destroy', to: 'sessions#destroy'
-
- 
-
-  root 'static_page#home'
+  root 'static_page#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
