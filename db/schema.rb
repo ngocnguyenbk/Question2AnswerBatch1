1bc74e0842702ae1f3e4346afe43b8d38541d619
linhfef2443445 <50351504+linhfef2443445@users.noreply.github.com>
2019-05-23 10:10:07 +0800

    * 50 Create comments model
    * 50 Create comments model
    
    * changing content validate
    
    * Fixing reference
    
    * add user reference with comments
    
    * fix polymophic associations
    
    * Revert

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190522033627) do
+ActiveRecord::Schema.define(version: 20190522093315) do
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text "content"
+    t.string "commentable_type"
+    t.bigint "commentable_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "username"
@@ -32,5 +43,6 @@ ActiveRecord::Schema.define(version: 20190522033627) do
     t.index ["user_id"], name: "index_work_experiences_on_user_id"
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "work_experiences", "users"
 end
