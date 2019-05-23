1e55d375008b49669a4cf661f891b699fbf0f914
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-23 15:06:42 +0700

    task 40 Login/Logout function

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,16 @@ class User < ApplicationRecord
   has_many :votes, as: :voteable 
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
   validates :username,  presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates :fullname, presence: true, length: { minimum: 20 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   enum role: [:manager, :member]
   has_secure_password
-
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+                                                    BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
-    end
+  end
 end
