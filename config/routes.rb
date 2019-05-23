e3bb6dfda80159c65d918659d732825740497048
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-23 15:06:42 +0700

    task 40 Login/Logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  get "/login",		to: "sessions#new"
+  post "/login",	to: "sessions#create"
+  delete  "/logout",	to: "sessions#destroy"
 
-	get "/login",		to: "sessions#new"
-	post "/login",	to: "sessions#create"
-	delete  "/logout",	to: "sessions#destroy"
+  root 'static_page#index'
 
-	root 'static_page#index'
-
-	resources :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
