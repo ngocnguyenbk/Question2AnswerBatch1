f28f29cfc7dd1c9729c2c57b97b2d114e4b3bf6b
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-23 14:26:06 +0700

    task 40 Login/Logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  root 'static_page#index'
+	get "/login",		to: "sessions#new"
+	post "/login",	to: "sessions#create"
+	delete  "/logout",	to: "sessions#destroy"
 
+	root 'static_page#index'
+
+	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20190524033903) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.integer "role"
     t.string "fullname"
     t.datetime "created_at", null: false
