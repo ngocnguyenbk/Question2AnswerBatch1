110d452f43901721c2beca16ff476fdf50b1e5c8
Linh <tomroket@gmail.com>
2019-05-23 15:42:35 +0800

    Indent 4

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
-	belongs_to :user
-	has_many :comments, as: :commentable
-	has_many :answers, dependent: :destroy
-	validates :title, presence: true, length: { maximum: 50 }
-	validates :content, presence: true
+  belongs_to :user
+  has_many :comments, as: :commentable
+  has_many :answers, dependent: :destroy
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true
 end
