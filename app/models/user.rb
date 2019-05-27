9c9bb8426c42831769ad5396f64e25957dbd1d32
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-27 16:21:09 +0700

    40-53

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   validates :username,  presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates :fullname, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   enum role: [:manager, :member]
   has_secure_password
 
