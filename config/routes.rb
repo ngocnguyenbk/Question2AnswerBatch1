48cf0c1ba8030ee331c3e5f12ef47eaa086097b4
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-27 14:46:54 +0700

    53

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get "/login",		to: "sessions#new"
   post "/login",	to: "sessions#create"
-  delete  "/logout",	to: "sessions#destroy"
+  delete  "/logout",to: "sessions#destroy"
 
-  root 'static_page#index'
+  root "static_page#index"
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
