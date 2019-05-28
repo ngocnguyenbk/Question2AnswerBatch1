ac8f8106fdb9477587123916e38c49379d86f57e
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-28 12:00:08 +0700

    Fix change password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
-  has_many :work_experiences, dependent: :destroy
+  has_secure_password
+
   attr_accessor :remember_token
-  before_save :downcase_email
+
+  has_many :work_experiences, dependent: :destroy
   has_many :questions, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :answers, dependent: :destroy
-  has_many :questions, dependent: :destroy
   has_many :votes, as: :voteable 
+
+  enum role: [:manager, :member]
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -14,8 +17,8 @@ class User < ApplicationRecord
   validates :fullname, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
-  enum role: [:manager, :member]
-  has_secure_password
+
+  before_save :downcase_email
 
   scope :login_scope, ->(text) {where("username = :text OR email = :text", text: text)}
   
@@ -33,8 +36,8 @@ class User < ApplicationRecord
   end
 
   def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+    self.remember_token = self.new_token
+    update_attribute(:remember_digest, self.digest(remember_token))
   end
 
   def authenticated?(remember_token)
