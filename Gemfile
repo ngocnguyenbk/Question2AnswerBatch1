c3b6e94437a8e6048d0c44aa3fee8a1aa9789e31
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-30 14:58:12 +0700

    asasa

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ end
 gem 'rails', '~> 5.1.7'
 gem 'bcrypt', '~> 3.1', '>= 3.1.12'
 gem 'faker',          '1.7.3'
+gem 'ransack'
 # Add boosttrap
 gem 'bootstrap-sass', '3.3.7'
 # Paginate
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  def index
+    @question = Question.all
+    @search = params[:search]
+    if @search.present?
+      @title = @search[:title]
+      @questions = Question.where(title: @title)      
+    end
+  end
+
   def logged_in_user
     return (redirect_to login_url; flash[:danger] = "Please log in.") if !logged_in?
   end
+
 end
