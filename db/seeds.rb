8f27e16ed5e16704f1a64b7eeee4294f33894000
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-05-30 08:44:06 +0700

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(username: "exampleuser",
+             email: "example@railstutorial.org",
+             password:    "123456",
+             fullname: "Nguyen Thi Anh Duong" 
+            )
+
+50.times do |n|
+  username  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "123456"
+  User.create!(username:  username,
+               email: email,
+               password: password,
+               fullname: "Nguyen Thi Anh Duong")
+end
+
+users = User.order(:created_at).take(6)
+50.times do |n|
+  title =  "Rails tutorial"
+  content = Faker::Lorem.sentence(5)
+  view = n + 1
+  users.each { |user| user.questions.create!(title: title, content: content, view: view) }
+end
+
+
+questions = Question.all
+user_id = User.all.to_a.sample.id
+voteable_type = "Questions"
+voteable_id = Question.all.to_a.sample.id
+questions.each { |question| question.votes.create!(voteable_type: voteable_type, voteable_id: voteable_id, user_id: user_id)}
